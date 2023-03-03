c749f1e139556ef755a1d52cb3f0b2cff916d486
Fool543 <120022950+Fool543@users.noreply.github.com>
2023-03-03 20:55:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,4 +9,5 @@ sudo dnf install dnf-plugins-core \
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo \
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc \
 sudo dnf install brave-browser \
+cp .xinitrc ~/.xinitrc \
 
